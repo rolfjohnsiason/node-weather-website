05027c6fcebcfb36c068109d17c63b54439332ff
Siason <rolf.john.s.siason@accenture.com>
2019-09-23 14:54:23 +0800

    added new informaton in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) => {
         }else if (body.error){
             callback('Unable to find location')
         }else{
-            callback(undefined,body.daily.data[0].summary + ' It is currently '+ body.currently.temperature +' degrees out. there is a '+body.currently.precipProbability+'% chance of rain')
+
+            callback(undefined,body.daily.data[0].summary + ' It is currently '+ body.currently.temperature +' degrees out. The High today is '+body.daily.data[0].temperatureHigh+' with a Low of ' + body.daily.data[0].temperatureLow +'. There is a '+body.currently.precipProbability+'% chance of rain')
         }
     })
     
